7bb8b662d4fe02e785725ccbe267b522c7242342
Jeff Chapman II <jac@JAChapmanII.net>
2011-06-24 19:21:16 -0400

    Start config repository with xmonad stuff

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,143 @@
+import XMonad
+import Data.Monoid
+import System.Exit
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+import XMonad.Actions.UpdatePointer
+import XMonad.Layout.Master
+
+myTerminal      = "xvt"
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+myBorderWidth   = 1
+
+myModMask       = mod4Mask
+
+myNumlockMask   = mod2Mask
+
+myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+
+-- light scheme
+myNormalBorderColor  = "#dddddd"
+-- dark scheme
+--myNormalBorderColor  = "#080808"
+
+myFocusedBorderColor = "#2d88b5"
+
+myLauncher = "dlauncher"
+
+------------------------------------------------------------------------
+-- Key bindings. Add, modify or remove key bindings here.
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm,               xK_p     ), spawn $ myLauncher)
+    , ((modm .|. shiftMask, xK_c     ), kill)
+
+    , ((modm,               xK_space ), sendMessage NextLayout)
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm,               xK_n     ), refresh)
+
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_m     ), windows W.focusMaster  )
+    , ((modm,               xK_Return), windows W.swapMaster)
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_l     ), sendMessage Expand)
+
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    ]
+    ++
+
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    ++
+
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+
+------------------------------------------------------------------------
+-- Mouse bindings: default actions bound to mouse events
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
+                                       >> windows W.shiftMaster))
+    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
+                                       >> windows W.shiftMaster))
+    ]
+
+------------------------------------------------------------------------
+-- Layouts
+myLayout = tiled ||| Mirror tiled ||| Full ||| nmtiled
+  where
+     tiled   = Tall nmaster delta ratio
+     -- Coding layout. Allows for (assuming Envy Code R 13):
+     -- (80-width term)[Filling Browser](80/80-width split tmux)
+     nmtiled = mastered (1/100) (81/400) $ Tall nmaster delta (49/100)
+
+     nmaster = 1
+     ratio   = 1/2
+     delta   = 1/100
+
+------------------------------------------------------------------------
+-- Window rules
+myManageHook = composeAll
+    [ className =? "MPlayer"        --> doFloat
+    , className =? "Gimp"           --> doFloat
+    , resource  =? "desktop_window" --> doIgnore
+    , resource  =? "kdesktop"       --> doIgnore ]
+
+------------------------------------------------------------------------
+-- Event handling
+myEventHook = mempty
+
+------------------------------------------------------------------------
+-- Status bars and logging
+myLogHook = updatePointer (Relative 0.5 0.5)
+
+------------------------------------------------------------------------
+-- Startup hook
+myStartupHook = return ()
+
+------------------------------------------------------------------------
+main = xmonad defaults
+
+defaults = defaultConfig {
+      -- simple stuff
+        terminal           = myTerminal,
+        focusFollowsMouse  = myFocusFollowsMouse,
+        borderWidth        = myBorderWidth,
+        modMask            = myModMask,
+        numlockMask        = myNumlockMask,
+        workspaces         = myWorkspaces,
+        normalBorderColor  = myNormalBorderColor,
+        focusedBorderColor = myFocusedBorderColor,
+
+      -- key bindings
+        keys               = myKeys,
+        mouseBindings      = myMouseBindings,
+
+      -- hooks, layouts
+        layoutHook         = myLayout,
+        manageHook         = myManageHook,
+        handleEventHook    = myEventHook,
+        logHook            = myLogHook,
+        startupHook        = myStartupHook
+    }
